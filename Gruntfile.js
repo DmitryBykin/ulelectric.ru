dc500563f0d8a860734a692c04ed1d1e7ff614fa
DmitryBykin <bykin2002@mail.ru>
2017-02-01 15:41:56 +0400

    init

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-csso");
+  grunt.loadNpmTasks("grunt-webp");
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({browsers: [
+              "last 2 versions"
+            ]})
+          ]
+        },
+        src: "css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "*.html",
+            "css/*.css"
+          ]
+        },
+        options: {
+          server: ".",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss"]
+      }
+    },
+    
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "css/style.min.css": ["css/style.css"]
+        }
+      }
+    },
+    
+    webp: {
+      files: {
+        expand: true,        
+        src: 'images/*.{png,gif,jpg}'
+        //dest: 'images/*'
+        //cwd: 'source-images/*.png'
+      },
+      options: {
+        binpath: require('webp-bin').path,
+        preset: 'photo',
+        verbose: true,
+        quality: 80,
+        alphaQuality: 80,
+        compressionMethod: 6,
+        segments: 4,
+        psnr: 42,
+        sns: 50,
+        filterStrength: 40,
+        filterSharpness: 3,
+        simpleFilter: true,
+        partitionLimit: 50,
+        analysisPass: 6,
+        multiThreading: true,
+        lowMemory: false,
+        alphaMethod: 0,
+        alphaFilter: 'best',
+        alphaCleanup: true,
+        noAlpha: false,
+        lossless: false
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch", "webp"]);
+  //grunt.registerTask('default', 'webp');
+};
