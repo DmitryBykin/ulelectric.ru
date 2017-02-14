23c4124f2e366f701645199a54165fa83245780c
DmitryBykin <bykin2002@mail.ru>
2017-02-14 13:31:19 +0400

    добавлен текстурный фон

---
+++ copy_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp -P 1979 css/* dmitry@ulelectric.ru:/home/web/ulelectric.ru/css/
+scp -P 1979 images/* dmitry@ulelectric.ru:/home/web/ulelectric.ru/images/
+scp -P 1979 *.html dmitry@ulelectric.ru:/home/web/ulelectric.ru/
